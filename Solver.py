e1429fe7960418ca08f357d078307863c67fdb1f
kaz1m1r <c.haan98@protonmail.com>
2021-03-17 19:56:43 -0400

    Added method that checks if a number is in one square. Also finished the solveSudoku method. Lastly removed some methods.

--- Solver.py
+++ Solver.py
@@ -1,8 +1,5 @@
-from typing import List
-
 from Grid import Sudokugrid
-
-import numpy as np
+from termcolor import colored
 
 
 class Sudokusolver:
@@ -14,34 +11,52 @@ class Sudokusolver:
         """
         self.sudoku: Sudokugrid = sudoku
 
-    def fillSudokuGrid(self):
+    def solveSudoku(self) -> None:
         """
         Fill the grid
         :return: None
         """
 
-        # if the board is correctly filled then stop the function call
-        if self.checkIfSquaresCorrectlyFilled():
+        # checks if the grid is filled
+        if self.checkSudokuFilled():
+            message = "\nSOLVED SUDOKU!\n"
+            print(colored(message, 'yellow'))
             self.sudoku.printGrid()
             return
 
-        else:
-            for y in range(0, 9):
-                for x in range(0, 9):
-                    if self.sudoku.grid[y][x] == 0:
-                        for number in range(1, 10):
+        # traversing rows
+        for y in range(0, 9):
 
-                            # check if the current number can be placed. If so, insert the number
-                            if self.canNumberBePlaced(y, x, number):
-                                self.insertNumber(y, x, number)
-                                self.fillSudokuGrid()
+            # traversing columns
+            for x in range(0, 9):
 
-                            else:
-                                continue
-                    else:
-                        continue
+                # check if position at the current x-coordinate and y-coordinate is empty
+                if self.sudoku.grid[y][x] == 0:
 
-    def canNumberBePlaced(self, y: int, x: int, number: int) -> bool:
+                    # traversing the numbers that could be placed in empty cell
+                    for number in range(1, 10):
+
+                        # check if the current number can be placed
+                        if self.checkRowCol(y, x, number) and self.checkSquare(y, x, number):
+
+                            self.insertNumber(y, x, number)  # insert the number
+                            self.solveSudoku()  # recursive call
+                            self.insertNumber(y, x, 0)  # place 0 again at the current position
+
+                        else:
+                            continue
+                    return
+
+    def checkSudokuFilled(self) -> bool:
+        """
+        Check if there are empty spaces in the grid
+        :return:
+        """
+        if 0 in self.sudoku.grid:
+            return False
+        return True
+
+    def checkRowCol(self, y: int, x: int, number: int) -> bool:
         """
         Check if a number can be placed on the specified coordinate
         :param y: y coordinate of the number
@@ -55,92 +70,38 @@ class Sudokusolver:
             return True
         return False
 
-    def checkIfSquaresCorrectlyFilled(self) -> bool:
+    def checkSquare(self, y: int, x: int, number: int) -> bool:
         """
-        Check if all of the squares in the grid are filled correctly. This
-        means that every square should contain the numbers 1-9
+        Check if the square that the number will be placed in already contains the number
+        :param y: y coordinate of where you want to place the number
+        :param x: x coordinate of where you want to place the number
+        :param number: number that you want to place at the coordinate
         :return: bool
         """
 
-        """ EXAMPLE OF A CELL:
+        """ 
+        EXAMPLE OF A SQUARE:
         -----------
-        | 1  4  5 | <- topRow
-        | 9  3  6 | <- middleRow
-        | 2  7  8 | <- bottomRow
+        | 1  4  5 |   <- top row
+        | 9  3  6 |   <- middle row
+        | 2  7  8 |   <- bottom row
         -----------
         """
 
-        for a in range(0, 9, 3):
-            for b in range(0, 9, 3):
+        # defining top left x coordinate of square and defining top left y coordinate of square
+        y_cord: int = (y // 3) * 3
+        x_cord: int = (x // 3) * 3
 
-                # making the top-, middle- and bottomRow of one of the 9 squares
-                topRow: np.array = [self.sudoku.grid[0 + a, b: 3 + b]]
-                middleRow: np.array = [self.sudoku.grid[1 + a, b: 3 + b]]
-                bottomRow: np.array = [self.sudoku.grid[2 + a, b: 3 + b]]
+        # traversing over rows
+        for y in range(y_cord, y_cord + 3):
 
-                # adding the top-, middle- and bottomRow to one array named 'cell'
-                cell: np.array = np.append(topRow, [middleRow, bottomRow])
+            # traversing over columns
+            for x in range(x_cord, x_cord + 3):
 
-                # returning False if the cell doesn't contain the numbers 1-9
-                for i in range(1, 10):
-                    if np.count_nonzero(cell == i) != 1:
-                        return False
-
-        # returning True if all the cells in the sudoku grid are filled and
-        # contain the numbers 1-9
+                # checking if a number in the cell is equal to the number
+                if self.sudoku.grid[y][x] == number:
+                    return False
         return True
-
-    def checkIfColumnCorrectlyFilled(self, x: int) -> bool:
-        """
-        Check if a column contains the numbers 1-9
-        :param x: x coordinate of the column that's checked
-        :return: bool
-        """
-
-        # saving the column to a variable
-        column: np.array = self.sudoku.grid[:, x]
-
-        # iterating over column to check if it contains numbers 1-9
-        for number in range(1, 10):
-            if column.count(number) != 1:
-                return False
-        return True
-
-    def checkIfRowCorrectlyFilled(self, y: int) -> bool:
-        """
-        Check if a row contains the numbers 1-9
-        :param y: y coordinate of the row that's checked
-        :return: Bool
-        """
-
-        # saving the specified row to a variable
-        row: np.array = self.sudoku.grid[y]
-
-        # iterating over the row to check if it contains numbers 1-9
-        for number in range(1, 10):
-            if row.count(number) != 1:
-                return False
-        return True
-
-    def checkForEmptyColumns(self, x: int) -> bool:
-        """
-        Only check if a column has cells that are not filled
-        :param x: x coordinate of the column
-        :return: bool
-        """
-        if 0 in self.sudoku.grid[:, x]:
-            return True
-        return False
-
-    def checkForEmptyRows(self, y: int) -> bool:
-        """
-        Only check if a row has cells that are not filled
-        :param y: y coordinate of the row
-        :return: bool
-        """
-        if 0 in self.sudoku.grid[y]:
-            return True
-        return False
 
     def insertNumber(self, y: int, x: int, number: int) -> None:
         """
